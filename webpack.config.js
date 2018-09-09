23b00455281f205f72f43dc0da49e99e1d5e3cc4
ramv13 <ramsai.vellanki@gmail.com>
2018-09-09 19:29:13 -0400

    Fixing webpack image loading

--- webpack.config.js
+++ webpack.config.js
@@ -25,7 +25,9 @@ module.exports = {
           loader: 'file-loader',
           options: {
             name: '[name].[ext]',
-            outputPath: 'img/'
+            outputPath: 'img/',
+            publicPath: '../img/',
+            useRelativePaths: true
           }
         }
       ]
